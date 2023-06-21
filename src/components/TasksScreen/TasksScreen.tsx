d7d9aa690d062189d6e7e3fbe20282b232d27fe7
Ch1py7 <garcialopezgerardo6612@gmail.com>
2023-06-20 22:15:02 -0600

    feat: create other screens without style

---
+++ src/components/TasksScreen/TasksScreen.tsx
@@ -0,0 +1,43 @@
+import { FC, ReactElement, useContext } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { DataContext } from '../../context/data.context'
+import { Options } from '../Options'
+
+export const TasksScreen: FC = (): ReactElement => {
+  const { tasksList } = useContext(DataContext)
+  return (
+    <View style={styles.container}>
+      <View style={styles.tasksContainer}>
+        {tasksList.map((task, index) => (
+          <View key={index} style={styles.tasks}>
+            <Text>{task}</Text>
+          </View>
+        ))}
+      </View>
+      <Options />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#2E1B1B',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+
+  tasksContainer: {
+    marginTop: 50,
+  },
+
+  tasks: {
+    height: 40,
+    width: 300,
+    backgroundColor: '#3F2C2C',
+    justifyContent: 'center',
+    paddingHorizontal: 14,
+    borderRadius: 10,
+    marginVertical: 10,
+  },
+})
---
+++ src/components/UserScreen/UserScreen.tsx
@@ -0,0 +1,20 @@
+import { FC, ReactElement } from 'react'
+import { StyleSheet, View } from 'react-native'
+import { Options } from '../Options'
+
+export const UserScreen: FC = (): ReactElement => {
+  return (
+    <View style={styles.container}>
+      <Options />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#2E1B1B',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+})
